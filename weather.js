19e145e1a60c4e8cc5e1dd47a94871ee481a7dbd
HBanana-1 <64995628+HBanana-1@users.noreply.github.com>
2021-03-22 11:17:15 -0500

    weather api

---
+++ weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "eb170e66e247d8d9c9e5dc017b380297";
+const COORDS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
+    ).then(function(response) {
+         return response.json();   
+    })
+    .then(function(json) {
+        const temp = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temp} @  ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("unable to access coords");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
